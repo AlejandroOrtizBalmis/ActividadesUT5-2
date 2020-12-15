77f69c9e75391adb73f72e98d04ad0fad9d406ba
Alextico 612 <alextico612@gmail.com>
2020-12-15 12:34:33 +0100

    Agregar archivos de proyecto.

---
+++ ActividadesUT5-2/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace ActividadesUT5_2
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> comida = new List<string> {"China","Mexicana","Americana" };
+            comboBox.ItemsSource = comida;
+        }
+    }
+}
